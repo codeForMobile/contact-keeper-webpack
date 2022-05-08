51a23049190adced8b76e7be9e5062cc8d0ffda2
codeForMobile <code.mobile@protonmail.com>
2022-05-08 22:51:12 +0300

    backend functionality complete

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+
+module.exports = function(req,res,next) {
+  const token = req.header('x-auth-token')
+
+  // if token not found
+  if(!token) {
+    return res.status(401).json({ msg : "token not found"})
+  }
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'))
+    req.user = decoded.user
+    next()
+  } catch (err) {
+    return res.status(401).json({ msg : "Invalid token"})
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,56 @@
+const bcrypt = require('bcryptjs');
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const config = require('config')
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  public
-router.post('/', (req,res) => {
-  res.send('Register a user')
+router.post('/', [
+  body('name', 'name is required').not().isEmpty(),
+  body('email', 'Email is required').isEmail(),
+  body('password', 'Password should be of length 6').isLength({ min: 6 })
+], async (req,res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty()) {
+    return res.status(404).json({ errors : errors.array()})
+  }
+
+  const { name, email, password } = req.body
+  try {
+    let user = await User.findOne({email})
+    if(user) {
+      return res.status(400).json({ msg : 'User already exists'})
+    }
+
+    user = new User({
+      name,
+      email,
+      password
+    })
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt)
+    await user.save()
+
+    const payload = {
+      user : {
+        id: user.id
+      }
+    }
+    jwt.sign(payload, config.get('jwtSecret'), {
+      expiresIn: 36000
+    }, (err, token) => {
+      if(err) throw err;
+      res.json({ token })
+    })
+    //res.send('passed')
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
 })
 
 module.exports = router
