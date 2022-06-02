e8a48ba86b527b55e764c0ca74436be5e1e2d20e
codeForMobile <code.mobile@protonmail.com>
2022-06-03 00:10:50 +0300

    Logout removing token correctly

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,7 +5,7 @@ import ContactFilter from '../contacts/ContactFilter'
 import AuthContext from '../../context/auth/authContext'
 
 const Home = () => {
-/*   const authContext = useContext(AuthContext)
+  /* const authContext = useContext(AuthContext)
 
   useEffect(() => {
     authContext.loadUser()
