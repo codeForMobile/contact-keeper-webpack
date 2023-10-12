8d7c633578dd6d65a5792d7d545c67e7d82e21f7
codeForMobile <code.mobile@protonmail.com>
2023-10-11 20:30:25 -0600

    add webpack, babel, loaders, plugins -- add loaders for babel, css, image.    move to webpack builds

---
+++ client/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+let mode = "development";
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+  }
+module.exports = {
+  mode: mode,
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path:path.resolve(__dirname, "build"),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    }
+  },
+  devtool: "source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: ["style-loader","css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+      },
+    ]
+  },
+}
